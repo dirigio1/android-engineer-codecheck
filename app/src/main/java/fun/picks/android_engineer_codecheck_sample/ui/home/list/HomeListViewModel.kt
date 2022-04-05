11d44800da3ecb711010e6b722b5346c785ea749
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-04-06 01:45:30 +0900

    create HomeListViewModel#init

--- app/src/main/java/fun/picks/android_engineer_codecheck_sample/ui/home/list/HomeListViewModel.kt
+++ app/src/main/java/fun/picks/android_engineer_codecheck_sample/ui/home/list/HomeListViewModel.kt
@@ -1,20 +1,42 @@
 package `fun`.picks.android_engineer_codecheck_sample.ui.home.list
 
+import `fun`.picks.android_engineer_codecheck_sample.R
 import `fun`.picks.android_engineer_codecheck_sample.data.model.Memo
 import `fun`.picks.android_engineer_codecheck_sample.data.repository.MemoRepository
+import androidx.annotation.VisibleForTesting
+import androidx.annotation.VisibleForTesting.PRIVATE
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class HomeListViewModel(
     private val memoRepository: MemoRepository
 ) : ViewModel() {
-    private val _uiModelFlow = MutableStateFlow<HomeListUiModel>(HomeListUiModel.Loading)
-    val uiModelFlow: StateFlow<HomeListUiModel>
-        get() = _uiModelFlow
+    private val _uiModelLD = MutableLiveData<HomeListUiModel>(HomeListUiModel.Loading)
+    val uiModelLD: LiveData<HomeListUiModel>
+        get() = _uiModelLD
+
+    private val _errorLD = MutableLiveData<Int>()
+    val errorLD: LiveData<Int>
+        get() = _errorLD
+
+    init { init() }
+
+    @VisibleForTesting(otherwise = PRIVATE)
+    fun init() {
+        viewModelScope.launch {
+            runCatching {
+                _uiModelLD.value = HomeListUiModel.MemoItem(memoRepository.getAllMemo())
+            }.onFailure {
+                _errorLD.value = R.string.error_occurred
+            }
+        }
+    }
 
     sealed class HomeListUiModel {
-        data class MemoItem(val memo: Memo) : HomeListUiModel()
+        data class MemoItem(val memos: List<Memo>) : HomeListUiModel()
         object Loading : HomeListUiModel()
     }
 }
