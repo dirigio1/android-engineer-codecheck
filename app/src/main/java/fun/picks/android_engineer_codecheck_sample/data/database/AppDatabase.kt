9dfc7ae138601c0a4e9bbaa7182cd49f1940e7c8
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-04-06 04:04:14 +0900

    Databaseを初期化できない問題解消

--- app/src/main/java/fun/picks/android_engineer_codecheck_sample/data/database/AppDatabase.kt
+++ app/src/main/java/fun/picks/android_engineer_codecheck_sample/data/database/AppDatabase.kt
@@ -1,10 +1,9 @@
 package `fun`.picks.android_engineer_codecheck_sample.data.database
 
-import `fun`.picks.android_engineer_codecheck_sample.data.model.Memo
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Memo::class], version = 1)
+@Database(entities = [MemoEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun memoDao(): MemoDao
 }
