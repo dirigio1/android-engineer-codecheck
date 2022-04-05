9dd1a819efeca0e973ee5201a1628e78d60f05f2
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-04-06 04:22:35 +0900

    HomeListViewModelのそれぞれのLiveDataを購読する処理実装

--- app/src/main/java/fun/picks/android_engineer_codecheck_sample/ui/home/list/HomeListFragment.kt
+++ app/src/main/java/fun/picks/android_engineer_codecheck_sample/ui/home/list/HomeListFragment.kt
@@ -1,12 +1,16 @@
 package `fun`.picks.android_engineer_codecheck_sample.ui.home.list
 
 import `fun`.picks.android_engineer_codecheck_sample.App
+import `fun`.picks.android_engineer_codecheck_sample.R
+import `fun`.picks.android_engineer_codecheck_sample.databinding.FragmentHomeListBinding
 import `fun`.picks.android_engineer_codecheck_sample.di.AppContainer
 import `fun`.picks.android_engineer_codecheck_sample.di.HomeListContainer
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 
-class HomeListFragment : Fragment() {
+class HomeListFragment : Fragment(R.layout.fragment_home_list) {
     companion object {
         fun newInstance() = HomeListFragment()
     }
@@ -22,8 +26,32 @@ class HomeListFragment : Fragment() {
         }
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentHomeListBinding.bind(view)
+
+        observeErrorLD()
+        observeUiModelLD(binding)
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         appContainer.homeListContainer = null
+    }
+
+    private fun observeErrorLD() {
+        viewModel.errorLD.observe(viewLifecycleOwner) { stringRes ->
+            AlertDialog.Builder(requireContext()).also {
+                it.setMessage(requireContext().getString(stringRes))
+                it.setPositiveButton(requireContext().getString(R.string.OK), null)
+                it.show()
+            }
+        }
+    }
+
+    private fun observeUiModelLD(binding: FragmentHomeListBinding) {
+        viewModel.uiModelLD.observe(viewLifecycleOwner) {
+
+        }
     }
 }
