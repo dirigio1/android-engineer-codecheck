0839808806d9d024c319f5608f44c400f3f85756
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-04-06 02:00:34 +0900

    apply ViewBinding

--- app/src/main/java/fun/picks/android_engineer_codecheck_sample/ui/home/HomeActivity.kt
+++ app/src/main/java/fun/picks/android_engineer_codecheck_sample/ui/home/HomeActivity.kt
@@ -1,12 +1,15 @@
 package `fun`.picks.android_engineer_codecheck_sample.ui.home
 
-import `fun`.picks.android_engineer_codecheck_sample.R
+import `fun`.picks.android_engineer_codecheck_sample.databinding.ActivityHomeBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
