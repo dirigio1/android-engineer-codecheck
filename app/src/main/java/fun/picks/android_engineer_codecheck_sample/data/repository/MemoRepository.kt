0fa84bf5539495f1f4b2ee8ddfa8a65832f1204c
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-04-06 00:32:23 +0900

    MemoRepositoryで関数を定義してMemoRepositoryImplで実装

--- app/src/main/java/fun/picks/android_engineer_codecheck_sample/data/repository/MemoRepository.kt
+++ app/src/main/java/fun/picks/android_engineer_codecheck_sample/data/repository/MemoRepository.kt
@@ -1,3 +1,8 @@
 package `fun`.picks.android_engineer_codecheck_sample.data.repository
 
-interface MemoRepository
+import `fun`.picks.android_engineer_codecheck_sample.data.model.Memo
+
+interface MemoRepository {
+    suspend fun getAllMemo(): List<Memo>
+    suspend fun insertAllMemo(vararg memos: Memo)
+}
--- app/src/main/java/fun/picks/android_engineer_codecheck_sample/data/repository/MemoRepositoryImpl.kt
+++ app/src/main/java/fun/picks/android_engineer_codecheck_sample/data/repository/MemoRepositoryImpl.kt
@@ -1,4 +1,18 @@
 package `fun`.picks.android_engineer_codecheck_sample.data.repository
 
-class MemoRepositoryImpl : MemoRepository {
+import `fun`.picks.android_engineer_codecheck_sample.data.database.AppDatabase
+import `fun`.picks.android_engineer_codecheck_sample.data.model.Memo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class MemoRepositoryImpl(
+    private val database: AppDatabase
+) : MemoRepository {
+    override suspend fun getAllMemo(): List<Memo> = withContext(Dispatchers.IO) {
+        database.memoDao().getAll()
+    }
+
+    override suspend fun insertAllMemo(vararg memos: Memo) = withContext(Dispatchers.IO) {
+        database.memoDao().insertAll(memos = memos)
+    }
 }
