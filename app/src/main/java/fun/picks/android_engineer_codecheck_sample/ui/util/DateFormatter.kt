de3e7bad6a8e1af0c8d1e9136a82816cb583b800
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-04-06 11:36:13 +0900

    fix DateFormatter#unixTimeToString

--- app/src/main/java/fun/picks/android_engineer_codecheck_sample/ui/util/DateFormatter.kt
+++ app/src/main/java/fun/picks/android_engineer_codecheck_sample/ui/util/DateFormatter.kt
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 fun unixTimeToString(unixTime: Long): String {
-    val dateFormatter = SimpleDateFormat("yyyy年MM月dd日", Locale.JAPAN)
+    val dateFormatter = SimpleDateFormat("yyyy年M月d日H時m分", Locale.JAPAN)
     val date = Date(unixTime * 1000)
     return dateFormatter.format(date)
 }
